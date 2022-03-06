56a08d430b3dc76edd8d6f1868aadae453eccef2
Adrien Delhorme <adrien.delhorme@gmail.com>
2022-03-06 23:09:48 +0100

    Force label colors in export

--- exports/export.sh
+++ exports/export.sh
@@ -1,4 +1,4 @@
-openscad -o main.png --imgsize 3000,6000 --colorscheme "Black & White" --projection o --camera 250,575,0,0,0,0,3002 ../modules/main.scad
-openscad -o large_room.png --imgsize 6000,4096 --colorscheme "Black & White" --projection o --camera 281,242,0,0,0,0,1421 ../modules/large_room.scad
-openscad -o small_room.png --imgsize 4096,4096 --colorscheme "Black & White" --projection o --camera 112,174,0,0,0,0,1036 ../modules/small_room.scad
-openscad -o stairs.png --imgsize 4096,4096 --colorscheme "Black & White" --projection o --camera 125,215,0,0,0,0,1279 ../modules/stairs.scad
+openscad -o main.png --imgsize 3000,6000 --colorscheme "Black & White" -D DIMENSION_COLOR='"black"' -D LABEL_COLOR='"black"' --projection o --camera 250,575,0,0,0,0,3002 ../modules/main.scad
+openscad -o large_room.png --imgsize 6000,4096 --colorscheme "Black & White"  -D DIMENSION_COLOR='"black"' -D LABEL_COLOR='"black"' --projection o --camera 281,242,0,0,0,0,1421 ../modules/large_room.scad
+openscad -o small_room.png --imgsize 4096,4096 --colorscheme "Black & White"  -D DIMENSION_COLOR='"black"' -D LABEL_COLOR='"black"' --projection o --camera 112,174,0,0,0,0,1036 ../modules/small_room.scad
+openscad -o stairs.png --imgsize 4096,4096 --colorscheme "Black & White"  -D DIMENSION_COLOR='"black"' -D LABEL_COLOR='"black"' --projection o --camera 125,215,0,0,0,0,1279 ../modules/stairs.scad
