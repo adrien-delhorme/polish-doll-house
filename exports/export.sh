78041d464cf4afb34240f2f03eb0e83584367875
Adrien Delhorme <adrien.delhorme@gmail.com>
2026-01-07 23:14:50 +0100

    MAINT: cleanup

--- exports/export.sh
+++
@@ -1,4 +0,0 @@
-openscad -o main_room.png --imgsize 3000,6000 --colorscheme "Black & White" -D DIMENSION_COLOR='"black"' -D LABEL_COLOR='"black"' --projection o --camera 250,575,0,0,0,0,3002 ../modules/main_room.scad
-openscad -o large_room.png --imgsize 6000,4096 --colorscheme "Black & White"  -D DIMENSION_COLOR='"black"' -D LABEL_COLOR='"black"' --projection o --camera 281,242,0,0,0,0,1421 ../modules/large_room.scad
-openscad -o small_room.png --imgsize 4096,4096 --colorscheme "Black & White"  -D DIMENSION_COLOR='"black"' -D LABEL_COLOR='"black"' --projection o --camera 112,174,0,0,0,0,1036 ../modules/small_room.scad
-openscad -o stairs.png --imgsize 4096,4096 --colorscheme "Black & White"  -D DIMENSION_COLOR='"black"' -D LABEL_COLOR='"black"' --projection o --camera 125,215,0,0,0,0,1279 ../modules/stairs.scad
